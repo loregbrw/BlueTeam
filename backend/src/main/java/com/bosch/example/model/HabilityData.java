89cb5c421f259b8ba984af55b3fe1868b1f6b97a
NiltonMeira <niltonprudys@gmail@gmail.com>
2024-07-15 13:02:12 -0300

    feat: UserData, HabilityData

---
+++ backend/src/main/java/com/bosch/example/model/HabilityData.java
@@ -0,0 +1,44 @@
+package com.bosch.example.model;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+import java.io.StringReader;
+
+import org.hibernate.annotations.Collate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "HabilityData")
+public class HabilityData {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "userId", referencedColumnName = "id")
+    private UserData userData;
+
+    @Column(name = "name")
+    private String name;
+
+    
+
+
+
+
+    
+}
