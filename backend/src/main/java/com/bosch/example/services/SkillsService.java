031899ab8fb171533e8c11a9bf6fafc32258f437
janeckiisa <janeckiisabelle@gmail.com>
2024-07-16 11:24:06 -0300

    SkillsService and SubjectService created.

---
+++ backend/src/main/java/com/bosch/example/services/SkillsService.java
@@ -0,0 +1,15 @@
+package com.bosch.example.services;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatusCode;
+
+import com.bosch.example.model.SkillsData;
+
+public interface SkillsService {
+    SkillsData PostSkill(Long id, Long SubjectClassId, String Name, String description, Integer weight);
+    List<SkillsData> GetSkillsSubjectClass(Long id);
+    SkillsData GetSkill(String name);
+    SkillsData PatchSkill(Long id, Long SubjectClassId, String Name, String description, Integer weight);
+    HttpStatusCode DeleteSkill(Long id);
+}
