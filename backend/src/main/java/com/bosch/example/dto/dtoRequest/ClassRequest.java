94ac3836281d126cf5a158b7f25a488e08b08489
Mavi <mariapimenta2607@gmail.com>
2024-07-17 14:09:45 -0300

    fix cors

--- backend/src/main/java/com/bosch/example/dto/dtoRequest/ClassRequest.java
+++ backend/src/main/java/com/bosch/example/dto/dtoRequest/ClassRequest.java
@@ -1,10 +1,11 @@
 package com.bosch.example.dto.dtoRequest;
 
+import java.time.LocalDate;
 import java.util.Date;
 
 public record ClassRequest(
     Long courseId, 
     String name, 
     Long duration, 
-    Date initialDate
+    LocalDate initialDate
 ) { }
