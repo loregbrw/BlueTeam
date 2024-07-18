08b90c8caa16c521c1cf4d83a025171deaa59c50
NiltonMeira <niltonprudys@gmail.com>
2024-07-18 12:21:49 -0300

    feat: added getClasses in signUp

--- frontend/src/pages/SignUp/components/FormSignUp/FormSignUp.tsx
+++ frontend/src/pages/SignUp/components/FormSignUp/FormSignUp.tsx
@@ -1,11 +1,27 @@
+import { useEffect, useState } from "react"
 import { StyledDropdown, StyledForm } from "../../../../components/loginForm/styled"
 import { StyledInput } from "../../../../components/loginForm/styled"
 import { MainContainer } from "../../../../components/loginForm/styled"
 import { StyledDateInput } from "../../../../components/loginForm/styled"
 import { StyledButton } from "../../../../components/loginForm/styled"
 import { StyledMiniBox } from "../../../../components/loginForm/styled"
+import { api } from "../../../../service/api"
 
 export const FormSignUp = () => {
+    const [classes, setClasses] = useState<string[]>([])
+
+    useEffect(() => {
+        const getClasses = async () =>{
+            try{
+                const response = await api.get(`classes`)
+                setClasses(response.data)
+            } catch(error){
+                setClasses([])
+            }
+        }
+        getClasses()
+    },[classes])
+
     return (
         <>
             <MainContainer>
@@ -24,10 +40,14 @@ export const FormSignUp = () => {
 
                     <StyledMiniBox>
                         <p>Turma</p>
-                        <StyledDropdown name="">
-                            <option value="opcao1">Opção 1</option>
-                            <option value="opcao2">Opção 2</option>
-                            <option value="opcao3">Opção 3</option>
+                        <StyledDropdown name="class" id="class">
+                        {
+                            classes.length > 0 
+                            ? classes.map((classItem, index) => (
+                                <option key={index} value={classItem.id}>{classItem.name}</option>
+                            ))
+                            : <option value="">Problema Interno</option>
+                        } 
                         </StyledDropdown>
                     </StyledMiniBox>
 
@@ -49,7 +69,7 @@ export const FormSignUp = () => {
                         <p>Data de nascimento</p>
                         <StyledDateInput />
                     </StyledMiniBox>
-                    
+
                     <StyledButton>Criar</StyledButton>
 
                 </StyledForm>
