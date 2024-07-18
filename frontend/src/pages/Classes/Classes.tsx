12c5a08018bd80398e996cbc8db54d7379a3e2fa
NiltonMeira <niltonprudys@gmail.com>
2024-07-18 16:36:55 -0300

    feat: reander classesPage

--- frontend/src/pages/Classes/Classes.tsx
+++ frontend/src/pages/Classes/Classes.tsx
@@ -1,27 +1,14 @@
 import { Card } from "./components/card/Card";
 import { StyledBox } from "./style";
-import { AdmBar } from "../../components/AdmBar/AdmBar";
 import { StyledInputCourses } from "./style";
 import { StyledInputDiv } from "./style";
 import lupa from "../../assets/lupa.png"
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { api } from "../../service/api";
 
-interface CardData {
-    id: number;
-    title: string;
-    content: string;
-    classes: string;
-}
 
-const cardData: CardData[] = [
-    { id: 1, title: 'Técnico em desenvolvimento de sistemas', content: 'Um técnico em desenvolvimento de sistemas é um profissional que desenvolve programas de computador, seguindo as especificações da lógica e das linguagens de programação.',classes: "Desenvolvimento de sistemas"},
-    { id: 2, title: 'Card 2', content: 'Content 2', classes: "Desenvolvimento de sistemas" },
-    { id: 3, title: 'Card 3', content: 'Content 3', classes: "Desenvolvimento de sistemas" },
-    { id: 3, title: 'Card 3', content: 'Content 3', classes: "Desenvolvimento de sistemas" },
-    // adicione mais cartões conforme necessário
-];
 
-export const Courses = () => {
+export const Classes = () => {
 
     interface classData {
         id: number,
@@ -36,8 +23,26 @@ export const Courses = () => {
         name: string,
         description: string | null
     }
-    
-    const [classes, setClasses] = useState
+
+    const [classes, setClasses] = useState<classData[]>([])
+
+    useEffect(() => {
+        const getClasses = async () => {
+            try {
+                const response = await api.get(`class`)
+                setClasses(response.data)
+            } catch (error) {
+                console.error(error);
+                setClasses([])
+            }
+        }
+        getClasses()
+    }, [])
+
+    const convertToBrazilianDate = (dateString: string): string => {
+        const date = new Date(dateString);
+        return date.toLocaleDateString('pt-BR');
+      };
 
     return (
         <>
@@ -47,14 +52,18 @@ export const Courses = () => {
                     <StyledInputCourses />
                     <img src={lupa} alt="" style={{ width: "30px", height: "30px" }} />
                 </div>
-                
+
             </StyledInputDiv>
 
             <div style={{ display: "flex", justifyContent: "center", overflow: "auto" }}>
                 <StyledBox>
-                    {cardData.map(card => (
-                        <Card key={card.id} title={card.title} content={card.content} classes={card.classes} />
-                    ))}
+                    {
+                        classes.map((classItem) => (
+                            <Card key={classItem.id} title={classItem.name} duration={classItem.duration} classes={classItem.courseId.name}
+                                initialDate={
+                                    convertToBrazilianDate(classItem.initialDate)} />
+                        ))
+                    }
                 </StyledBox>
 
             </div>
--- frontend/src/pages/Classes/components/card/Card.tsx
+++ frontend/src/pages/Classes/components/card/Card.tsx
@@ -4,20 +4,21 @@ import { StyledCard } from "./style";
 
 interface CardProps {
     title: string;
-    content: string;
+    duration: number;
     classes: string;
+    initialDate: string;
 }
 
-export const Card: React.FC<CardProps> = ({ title, content, classes}) => {
+export const Card: React.FC<CardProps> = ({ title, duration, classes, initialDate}) => {
     return(
         <>  
             <StyledLink to="">
                 <StyledCard>
                     <h3>{title}</h3>
                     <hr  style={{width: "100%"}}/>
-                    <p>{content}</p>
-                    <p style={{color: "#005691"}}>{classes}</p>
-
+                    <p> Duração: {duration} horas</p>
+                    <p style={{color: "#005691"}}>Curso: {classes}</p>
+                    <p>Data de inicio: {initialDate}</p>
                 </StyledCard>
             </StyledLink>
         </>
