61a82134c8b998aca8aef60c90f69993bf5f667e
Mavi <mariapimenta2607@gmail.com>
2024-07-19 15:40:32 -0300

    added progressBar

--- frontend/src/pages/ChangePassword/style.ts
+++ frontend/src/pages/ChangePassword/style.ts
@@ -52,3 +52,31 @@ export const MainContainer = styled.div`
 
     align-items: center;
 `
+
+export const PasswordWarning = styled.span`
+  position: absolute;
+  bottom: -20px;
+  left: 50%;
+  transform: translate(-50%, 0);
+  text-align: center;
+  color: #c93d50;
+  width: 90%;
+`;
+
+export const ProgressBar = styled.div<{ fillValue: number; backgroundClass: string }>`
+  height: 10px;
+  border-radius: 50px;  
+  transition: width 0.3s ease-in-out;  
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.1); 
+  width: ${(props) => props.fillValue}%;
+  background-color: ${(props) =>
+    props.backgroundClass === "bg-danger"
+      ? "#dc3545"
+      : props.backgroundClass === "bg-warning"
+      ? "#ffc107"
+      : props.backgroundClass === "bg-secondary"
+      ? "#6c757d"
+      : props.backgroundClass === "bg-info"
+      ? "#17a2b8"
+      : "#28a745"};
+`;
